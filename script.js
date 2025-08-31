015e741a24ddf5553b9fd25ef6eeaca6eea77224
beto0077 <131410367+beto0077@users.noreply.github.com>
2025-08-31 01:39:07 -0600

    More styles were applied to the HTML using CSS to create a more comfortable visual base to work with, and the base code was created to generate the cards for each book.

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const testText = document.createElement("p");
 testText.textContent = "The JS is working as well...";
 testDiv.appendChild(testText);
 //------------------------------------------------------------
+const cardsContainer = document.querySelector(".cards-container");
 const bookList = [];
 
 function addNewLine(newText, typeElement) {
@@ -33,9 +34,20 @@ function Book(title, author, pages, hasBeenRead) {
     }
 }
 
+Book.prototype.changeReadStatus = function () {
+    this.hasBeenRead = this.hasBeenRead ? false : true;
+}
+
 function addBookToLibrary(title, author, pages, hasBeenRead) {
     const newBook = new Book(title, author, pages, hasBeenRead);
     bookList.push(newBook);
+}
+
+function deleteBookFromLibrary(bookId) {
+    const indexToDelete = bookList.findIndex(item => item.id === bookId);
+    if (indexToDelete !== -1) {
+        bookList.splice(indexToDelete, 1);   
+    }
 }
 
 addBookToLibrary("1984", "George Orwell", 328, true);
@@ -52,4 +64,44 @@ addBookToLibrary("The Lord of the Rings", "J.R.R. Tolkien", 1178, true);
 addNewLine("List of books on library:", "h2")
 bookList.forEach(book => {
     addNewLine(`ID: ${book.id} / Info: ${book.info()}`);
+});
+
+function createCatalogCard(book) {
+    const cardItem = document.createElement("div");
+    const titleData = document.createElement("h3");
+    const authorData = document.createElement("p");
+    const pagesData = document.createElement("p");
+    const readBox = document.createElement("div");
+    const readText = document.createElement("p");
+    const readButton = document.createElement("button");
+    const deleteButton = document.createElement("button");
+    
+    cardItem.id = book.id;
+    cardItem.classList.add("card-item");
+    titleData.textContent = book.title;
+    authorData.textContent = `Author: ${book.author}`;
+    pagesData.textContent = `Pages: ${book.pages}`;
+    readText.textContent = "Has it been read?";
+    readButton.classList.add("read-button");
+    readButton.dataset.choice = "toggleRead";
+    readButton.textContent = book.hasBeenRead ? "Read ✓" : "Not Read ✗";
+    deleteButton.classList.add("delete-button");
+    deleteButton.dataset.choice = "delete";
+    deleteButton.textContent = "Delete";
+
+    cardItem.appendChild(titleData);
+    cardItem.appendChild(authorData);
+    cardItem.appendChild(pagesData);
+    readBox.appendChild(readText);
+    readBox.appendChild(readButton);
+    cardItem.appendChild(readBox);
+    cardItem.appendChild(deleteButton);
+
+    return cardItem;
+}
+
+bookList.forEach(book => {
+    const newCard = createCatalogCard(book);
+    cardsContainer.appendChild(newCard);
+    console.log(newCard);
 });
